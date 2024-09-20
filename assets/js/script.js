51cd5fc11ff451a95295f04b6a4b70bc0498f66a
jhon fredy <joberdigital6@gmail.com>
2024-09-19 21:57:08 -0500

    ordeno la salida en objeto

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,13 @@
-
-let listaNombresGastos = [];
-let listaValoresGastos = [];
-
-//Esta funcion se inicia al momento de el que usuario hace click en el boton
+let __listaNombresGastos = [];
 function clickBoton(){
     let nombreGasto = document.getElementById('nombreGasto').value;
     let valorGasto = document.getElementById('valorGasto').value;
- 
-    listaNombresGastos.push(nombreGasto);
-    listaValoresGastos.push(valorGasto);
-
-    console.log(listaNombresGastos);
-    console.log(listaValoresGastos);
+    const objeto ={
+        nombreGasto,
+        valorGasto,
+            }
+    __listaNombresGastos.push({nombreGasto,valorGasto});
+    console.log(__listaNombresGastos);
      alert('clik del usuario');
 
 document.getElementById('nombreGasto').value=""  
