75cc072a7175d9bbf1a427400a842e99914a8101
jhon fredy <joberdigital6@gmail.com>
2024-09-19 22:15:00 -0500

    no repetir un dato dentro de el array

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,19 @@ let __listaNombresGastos = [];
 function clickBoton(){
     let nombreGasto = document.getElementById('nombreGasto').value;
     let valorGasto = document.getElementById('valorGasto').value;
-    const objeto ={
+const objeto ={
         nombreGasto,
         valorGasto,
             }
-    __listaNombresGastos.push({nombreGasto,valorGasto});
-    console.log(__listaNombresGastos);
-     alert('clik del usuario');
-
+__listaNombresGastos.push({nombreGasto,valorGasto});
+let personasUnicas = __listaNombresGastos.reduce((unique, item) => {
+        if (!unique.some(obj => obj.nombreGasto === item.nombreGasto)) {
+            unique.push(item);
+        }
+        return unique;
+    }, []);
+    console.log(personasUnicas);
+ alert('clik del usuario');
 document.getElementById('nombreGasto').value=""  
 document.getElementById('valorGasto').value=""
 }
